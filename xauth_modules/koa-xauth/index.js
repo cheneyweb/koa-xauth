c7cddbe32a5db535e94eb747124c0e346c7218d4
cheneyweb <457299596@qq.com>
2017-12-06 15:30:55 +0800

    精简async语法

--- xauth_modules/koa-xauth/index.js
+++ xauth_modules/koa-xauth/index.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken')
 const log = require('tracer').colorConsole()
 
 const xauth = {
-    async init(router, authConfig) {
-        router.use(async function (ctx, next) {
+    init(router, authConfig) {
+        router.use(function (ctx, next) {
             try {
                 // 非白名单，进入TOKEN校验
                 if (authConfig.pass.indexOf(ctx.url) == -1) {
-                    const tokenVerify = await jwt.verify(ctx.header.token, authConfig.secret)
+                    const tokenVerify = jwt.verify(ctx.header.token, authConfig.secret)
                     if (tokenVerify) {
                         ctx.tokenVerify = tokenVerify
                         next()
