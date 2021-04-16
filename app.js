ef0c31cd765f248b0082e24552ccbbff1d543aa5
MeiBin <meibin_whu@163.com>
2021-04-16 14:30:10 +0800

    optimize:优化中间件性能：预先创建正则表达式对象，避免频繁创建对象

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // 系统配置参数
 const config = require('config')
 const port = config.server.port
+
 // 应用服务相关
 const Koa = require('koa')
 const koaBody = require('koa-body')
 const jwt = require('jsonwebtoken')
-const xauth = require(__dirname + '/xauth_modules/koa-xauth/index.js')
+const xauth = require('./xauth_modules/koa-xauth/index.js')
+
 // 日志相关
 const log = require('tracer').colorConsole({ level: config.log.level })
+
 // 路由相关
 const Router = require('koa-router')
 const router = new Router()
@@ -15,52 +18,47 @@ const router = new Router()
 // 初始化应用服务
 const app = new Koa()
 app.use(koaBody())
-app.use(xauth(config.auth, (v) => v, (ctx) => {    // 参数1：认证配置，参数2：TOKEN提取规则，参数3：自定义错误处理
-    if (ctx.body.name == 'TokenExpiredError') {
-        ctx.body.code = -1
-        ctx.body.msg = '自定义错误信息，TOKEN已过期'
-    }
+app.use(xauth(config.auth, (v) => v, (ctx) => { // 参数1：认证配置，参数2：TOKEN提取规则，参数3：自定义错误处理
+  if (ctx.body.name === 'TokenExpiredError') {
+    ctx.body.code = -1
+    ctx.body.msg = '自定义错误信息，TOKEN已过期'
+  }
 }))
 app.use(router.routes())
 
 // ===== 开始：用户认证中间件例子，‘/auth’已经配置白名单，‘/test’路由受保护 =====
 // 1、模拟用户登录，生成加密TOKEN令牌
 router.use('/auth', async function (ctx, next) {
-    try {
-        if (true) { // 判断用户名密码等认证方式，这里默认通过
-            const user = { userId: '123', role: 'admin' }
-            const tokenSign = await jwt.sign({  // exp设置过期时间，这里是24小时
-                ...user, iat: Math.floor(Date.now() / 1000), exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60),
-            }, config.auth.secret)
-            ctx.tokenSign = tokenSign // 向后面的路由传递TOKEN加密令牌
-            next()
-        } else {
-            ctx.status = 401
-            ctx.body = '用户名或密码错误'
-        }
-    } catch (error) {
-        console.error(error)
-        ctx.body = error
-    }
+  try {
+    const user = { userId: '123', role: 'admin' }
+    const tokenSign = await jwt.sign({ // exp设置过期时间，这里是24小时
+      ...user, iat: Math.floor(Date.now() / 1000), exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60)
+    }, config.auth.secret)
+    ctx.tokenSign = tokenSign // 向后面的路由传递TOKEN加密令牌
+    next()
+  } catch (error) {
+    console.error(error)
+    ctx.body = error
+  }
 })
 // 2、向前端传递TOKEN加密令牌
 router.get('/auth', async function (ctx, next) {
-    ctx.body = ctx.tokenSign
+  ctx.body = ctx.tokenSign
 })
 // 3、下次其余路由需要在请求时在header中加上token参数，如果没有token或者token错误，xauth中间件会提示错误
 router.get('/test', async function (ctx, next) {
-    ctx.body = ctx.tokenVerify // 获取TOKEN解析结果
+  ctx.body = ctx.tokenVerify // 获取TOKEN解析结果
 })
 
 // 路由角色控制
 router.get('/financial/test1', async function (ctx, next) {
-    ctx.body = ctx.tokenVerify
+  ctx.body = ctx.tokenVerify
 })
 router.post('/financial/test1', async function (ctx, next) {
-    ctx.body = ctx.tokenVerify
+  ctx.body = ctx.tokenVerify
 })
 router.get('/financial/test2', async function (ctx, next) {
-    ctx.body = ctx.tokenVerify
+  ctx.body = ctx.tokenVerify
 })
 // ===== 结束：用户认证中间件例子，‘/auth’已经配置白名单，‘/test’路由受保护 =====
 
--- xauth_modules/koa-xauth/index.js
+++ xauth_modules/koa-xauth/index.js
@@ -1,109 +1,138 @@
+/* eslint-disable no-throw-literal */
 const jwt = require('jsonwebtoken')
 const log = require('tracer').colorConsole()
 
 global._tokenMap = {}
 
 module.exports = function (authConfig = {}, tokenRule, errorProcess) {
-    return function xauth(ctx, next) {
-        authConfig.tokenname = authConfig.tokenname || 'token'
-        authConfig.pass = authConfig.pass || []
-        authConfig.errMsg = authConfig.errMsg || '未认证'
-        authConfig.errStatus = authConfig.errStatus || 401
-        authConfig.errMutexMsg = authConfig.errMutexMsg || '身份已过期，请重新登录'
+  authConfig.tokenname = authConfig.tokenname || 'token'
+  authConfig.pass = authConfig.pass || []
+  authConfig.errMsg = authConfig.errMsg || '未认证'
+  authConfig.errStatus = authConfig.errStatus || 401
+  authConfig.errMutexMsg = authConfig.errMutexMsg || '身份已过期，请重新登录'
 
-        // 是否放行跨域OPTIONS请求
-        if (authConfig.cors && ctx.method == 'OPTIONS') {
-            return next()
-        }
-        // 白名单内，进行标记
-        let isPass = false
-        if (authConfig.pass && authConfig.pass instanceof Array && authConfig.pass.length > 0) {
-            for (let p of authConfig.pass) {
-                const rep = new RegExp(p)
-                if (rep.test(ctx.url)) {
-                    isPass = true
-                    break
-                }
-            }
-        }
-        // 从请求中获取TOKEN令牌
-        let token = ctx.header[authConfig.tokenname] || ctx.header.token
-        if (tokenRule) {
-            token = tokenRule(token)
-        }
-        // 白名单，且无token，直接通过返回
-        if (isPass && !token) {
-            return next()
-        }
-        // 进行校验
-        try {
-            const tokenVerify = jwt.verify(token, authConfig.secret)
-            if (isPass) {
-                ctx.tokenVerify = tokenVerify
-                return next()
-            }
-            if (tokenVerify) {
-                // 判断是否单点登录
-                if (authConfig.mutex) {
-                    let tokenKey = `${tokenVerify.role}${tokenVerify.id}`
-                    // 内存中没有这个TOKEN，则直接赋值
-                    if (!_tokenMap[tokenKey] || _tokenMap[tokenKey].iat < tokenVerify.iat) {
-                        _tokenMap[tokenKey] = tokenVerify
-                    }
-                    // 检查传入TOKEN和内存中的TOKEN是否一致
-                    if (_tokenMap[tokenKey].iat > tokenVerify.iat) {
-                        ctx.status = authConfig.errStatus
-                        throw { res: authConfig.errMutexMsg }
-                    }
-                }
-                // 未配置角色控制，跳过
-                if (!authConfig.role) {
-                    ctx.tokenVerify = tokenVerify
-                    return next()
-                }
-                // 已配置角色控制，则进一步进行角色身份检查，遍历角色所拥有的路由规则
-                let roleRegArr = tokenVerify.role ? authConfig.role[tokenVerify.role] : null
-                if (roleRegArr && roleRegArr instanceof Array && roleRegArr.length > 0) {
-                    for (let item of roleRegArr) {
-                        // 首先判断请求method是否匹配
-                        if (item.indexOf(':') > 0) {
-                            let itemArr = item.split(':')
-                            if (itemArr[0] != ctx.method) {
-                                continue
-                            }
-                            item = itemArr[1]
-                        }
-                        // 其次判断请求url是否匹配
-                        const re = new RegExp(item)
-                        if (re.test(ctx.url)) {
-                            ctx.tokenVerify = tokenVerify
-                            return next()
-                        }
-                    }
-                    // 失败：所有路由规则循环完毕均不能匹配
-                    throw { res: `角色[${tokenVerify.role}]未拥有访问权限` }
-                }
-                // 失败：角色拥有路由规则为空
-                else {
-                    throw { res: `角色[${tokenVerify.role}]未配置访问权限` }
-                }
-            }
-            // 失败：TOKEN解析失败
-            else {
-                throw { res: authConfig.errMsg }
-            }
-        } catch (error) {
-            // 白名单接口忽略错误
-            if (isPass) {
-                return next()
-            }
-            ctx.status = authConfig.errStatus
-            ctx.body = { err: true, ...error }
-            log.error(error)
-            // 额外可选错误处理
-            if (errorProcess && typeof (errorProcess) == 'function') {
-                errorProcess(ctx)
-            }
-        }
+  /* 预先创建rules的正则表达式 */
+
+  /**
+   * @type {RegExp[]} 白名单正则表达式列表
+   */
+  let passRegList = []
+  if (authConfig.pass && Array.isArray(authConfig.pass)) {
+    passRegList = authConfig.pass.map(it => new RegExp(it))
+  }
+
+  /**
+   * @type {{[role:string]: {method: string, reg: RegExp}[]}}
+   * 每个角色的正则表达式列表
+   */
+  const roleRegMap = {}
+  if (authConfig.role) {
+    Reflect.ownKeys(authConfig.role).forEach(key => {
+      if (Array.isArray(authConfig.role[key])) {
+        const roleConf = authConfig.role[key].map(p => {
+          if (p.indexOf(':') > -1) {
+            const [method, path] = p.split(':')
+            return { method, reg: new RegExp(path) }
+          } else {
+            return { method: null, reg: new RegExp(p) }
+          }
+        })
+        roleRegMap[key] = roleConf
+      }
+    })
+  }
+
+  return function xauth (ctx, next) {
+    // 是否放行跨域OPTIONS请求
+    if (authConfig.cors && ctx.method === 'OPTIONS') {
+      return next()
     }
+
+    // 白名单内，进行标记
+    let isPass = false
+    if (passRegList.length > 0) {
+      if (passRegList.find(reg => reg.test(ctx.url))) {
+        isPass = true
+      }
+    }
+
+    // 从请求中获取TOKEN令牌
+    let token = ctx.header[authConfig.tokenname] || ctx.header.token
+    if (tokenRule) {
+      token = tokenRule(token)
+    }
+
+    // 白名单，且无token，直接通过返回
+    if (isPass && !token) {
+      return next()
+    }
+
+    // 进行校验
+    try {
+      const tokenVerify = jwt.verify(token, authConfig.secret)
+
+      if (isPass) {
+        ctx.tokenVerify = tokenVerify
+        return next()
+      }
+
+      if (tokenVerify) {
+        // 判断是否单点登录
+        if (authConfig.mutex) {
+          const tokenKey = `${tokenVerify.role}${tokenVerify.id}`
+          // 内存中没有这个TOKEN，则直接赋值
+          if (!global._tokenMap[tokenKey] || global._tokenMap[tokenKey].iat < tokenVerify.iat) {
+            global._tokenMap[tokenKey] = tokenVerify
+          }
+          // 检查传入TOKEN和内存中的TOKEN是否一致
+          if (global._tokenMap[tokenKey].iat > tokenVerify.iat) {
+            ctx.status = authConfig.errStatus
+            throw { res: authConfig.errMutexMsg }
+          }
+        }
+
+        // 未配置角色控制，跳过
+        if (!authConfig.role) {
+          ctx.tokenVerify = tokenVerify
+          return next()
+        }
+
+        // 已配置角色控制，则进一步进行角色身份检查，遍历角色所拥有的路由规则
+        const roleRegs = tokenVerify.role ? roleRegMap[tokenVerify.role] : null
+        if (roleRegs && roleRegs.length > 0) {
+          for (const { method, reg } of roleRegs) {
+            // 首先判断请求method是否匹配
+            if (method && ctx.method !== method) {
+              continue
+            }
+            // 其次判断请求url是否匹配
+            if (reg.test(ctx.url)) {
+              ctx.tokenVerify = tokenVerify
+              return next()
+            }
+          }
+          // 失败：所有路由规则循环完毕均不能匹配
+          throw { res: `角色[${tokenVerify.role}]未拥有访问权限` }
+        } else {
+          // 失败：角色拥有路由规则为空
+          throw { res: `角色[${tokenVerify.role}]未配置访问权限` }
+        }
+      } else {
+        // 失败：TOKEN解析失败
+        throw { res: authConfig.errMsg }
+      }
+    } catch (error) {
+      // 白名单接口忽略错误
+      if (isPass) {
+        return next()
+      }
+      log.error(error)
+      ctx.status = authConfig.errStatus
+      ctx.body = { err: true, ...error }
+      // 额外可选错误处理
+      if (errorProcess && typeof errorProcess === 'function') {
+        errorProcess(ctx)
+      }
+    }
+  }
 }
